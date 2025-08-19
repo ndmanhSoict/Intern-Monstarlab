1d0e843f234b766a0d8f54f4edbaeb90716e7584
ndmanhSoict <manhsoict@gmail.com>
2025-08-19 16:55:44 +0700

    update bài 3

--- bai_3.js
+++ bai_3.js
@@ -1,8 +1,23 @@
 function itemBoth(arr1, arr2) {
     let result = [];
     let getItem1 = new Set(arr1);
+    let getItem2 = new Set(arr2);
     for (x of getItem1) {
-        if (arr2.includes(x)) {
+        if (typeof x == 'object') {
+            outerLoop:
+            for (y of getItem2) {
+                if (typeof y != 'object') continue;
+                else {
+                    for (key in x) {
+                        if (x[key] !== y[key]) {
+                            continue outerLoop;
+                        }
+                    }
+                    result.push(x);
+                }
+            }
+        }
+        else if (arr2.includes(x)) {
             result.push(x);
         }
     }
@@ -14,3 +29,13 @@ function itemBoth(arr1, arr2) {
 // console.log(itemBoth(["apple", "banana", "cherry"], ["banana", "date", "cherry"]));
 // console.log(itemBoth([true, false, true], [false, false]));
 // console.log(itemBoth([1, "1", true, null, undefined], ["1", false, 1, null]));
+
+
+// const arr1 = [1, 7, 2, { id: 1, name: "A" }, { id: 2, name: "B" }, 3];
+// const arr2 = [2, 3, { id: 1, name: "A" }, { id: 3, name: "C" }, { id: 2, name: "B" }, 5, 7];
+// console.log(itemBoth(arr1, arr2));
+
+// const arr11 = [1, "a", true, { id: 1 }, { id: 2 }];
+// const arr22 = [2, "a", false, { id: 1 }, { id: 3 }];
+// console.log(itemBoth(arr11, arr22));
+// itemBoth(arr11, arr22);
